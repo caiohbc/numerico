b58e76b95f02e5fa1b04d7fcc829e0305fbd99b7
Caio Boigues <caiohbc@gmail.com>
2025-08-22 18:44:50 -0400

    changing indentation

--- interpolacao.py
+++ interpolacao.py
@@ -1,30 +1,29 @@
 import numpy as np
 
-# Interpolação linear, gerando 13 retas
+# Interpolação linear, gerando 12 retas
 # Integrar as retas usando método de simpson nos intervalos correspondentes
-# Pensar em uma maneira de considerar retas cuja área está fora do polígono
+# Considerar corretamente retas cuja área está fora do polígono
 # Calcular a diferença entre o valor interpolado e o valor real
 
 def get_points(path: str):
-  points_array = np.genfromtxt(path, delimiter=',', dtype=[('x', np.float64), ('y', np.float64)])
-  return points_array
+    points_array = np.genfromtxt(path, delimiter=',', dtype=[('x', np.float64), ('y', np.float64)])
+    return points_array
 
 def first_degree_interpolation(p0: tuple, p1: tuple):
-  m = (p1['y'] - p0['y'])/(p1['x'] - p0['x'])
-  b = ((p1['x'] * p0['y']) - (p0['x'] * p1['y']))/(p1['x'] - p0['x'])
+    m = (p1['y'] - p0['y'])/(p1['x'] - p0['x'])
+    b = ((p1['x'] * p0['y']) - (p0['x'] * p1['y']))/(p1['x'] - p0['x'])
 
-  line = np.array((m, b), dtype=[('m', np.float64), ('b', np.float64)])
+    line = np.array((m, b), dtype=[('m', np.float64), ('b', np.float64)])
     
-  return line
+    return line
 
 POINTS_PATH = 'points.csv'
-
 points = get_points(POINTS_PATH)
 
 lines = np.array([], dtype=[('m', np.float64), ('b', np.float64)])
 
 for i in range(len(points) - 1):
-  line = first_degree_interpolation(points[i], points[i+1])
-  lines = np.append(lines, line)
+    line = first_degree_interpolation(points[i], points[i+1])
+    lines = np.append(lines, line)
 
 print(lines)
